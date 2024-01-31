a33f8b8fae51abf6b35ebfd7d9ea2878f02c9c06
Sara <125512367+Saraiin@users.noreply.github.com>
2024-01-31 12:01:30 +0000

    style for task 2 completed

--- 0x01-react_intro/task_2/dashboard/src/Notifications.js
+++ 0x01-react_intro/task_2/dashboard/src/Notifications.js
@@ -5,6 +5,11 @@ function Notifications() {
     return (
         <div className='Notifications'>
             <p>Here is the list of notifications</p>
+            <ul>
+                <li id="blue">New course available</li> 
+                <li>New resume available</li> 
+                <li><b>Urgent requirement</b> - complete by EOD</li> 
+            </ul>
         </div>
     )
 }
